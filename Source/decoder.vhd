3204b2bc87ecaab3891e88fb4a14a05b992e753c
TimeEngineer <36041853+TimeEngineer@users.noreply.github.com>
2018-03-09 20:03:24 +0100

    back up

--- Source/decoder.vhd
+++ Source/decoder.vhd
@@ -23,12 +23,6 @@ signal Posit2 		: std_logic_vector (7 downto 0);
 signal Start0 		: std_logic;
 signal Start1 		: std_logic;
 signal Start2 		: std_logic;
-signal tempVector0	: std_logic_vector (7 downto 0);
-signal tempVector1	: std_logic_vector (7 downto 0);
-signal tempVector2	: std_logic_vector (7 downto 0);
-signal temp0		: std_logic;
-signal temp1		: std_logic;
-signal temp2		: std_logic;
 signal input_Error 	: std_logic;
 
 component fdiv port(
@@ -84,23 +78,11 @@ component servo2 port(
 );
 end component;
 
-component loop0 port(
-  clk     		: in std_logic;
-  input   		: in std_logic_vector (7 downto 0);
-  dataValid		: in std_logic;
-  rst			: in std_logic;
-  output		: out std_logic_vector (7 downto 0);
-  start			: out std_logic);
-end component;
-
 begin
 C0 : fdiv 	port map(Clk => Clk, Rst => Rst, Tick7us => Tick);
 C1 : MAE_servo 	port map(clk => Clk, input => Input, go => Start, rst => Rst,
-			output0 => tempVector0, output1 => tempVector1, output2 => tempVector2,
-			dataValid0 => temp0, dataValid1 => temp1, dataValid2 => temp2, input_Error => input_Error);
-C2 : loop0	port map(clk => tick, input => tempVector0, dataValid => temp0, rst => Rst, output => Posit0, start => Start0); 
-C3 : loop0	port map(clk => tick, input => tempVector1, dataValid => temp1, rst => Rst, output => Posit1, start => Start1);
-C4 : loop0	port map(clk => tick, input => tempVector2, dataValid => temp2, rst => Rst, output => Posit2, start => Start2);
+			output0 => Posit0, output1 => Posit1, output2 => Posit2,
+			dataValid0 => Start0, dataValid1 => Start1, dataValid2 => Start2, input_Error => input_Error);
 C5 : servo0 	port map(Clk => Clk, Rst => Rst, Tick => Tick, Start => Start0, Posit => Posit0, Done => Done0, Q => Q0);
 C6 : servo1 	port map(Clk => Clk, Rst => Rst, Tick => Tick, Start => Start1, Posit => Posit1, Done => Done1, Q => Q1);
 C7 : servo2 	port map(Clk => Clk, Rst => Rst, Tick => Tick, Start => Start2, Posit => Posit2, Done => Done2, Q => Q2); 
