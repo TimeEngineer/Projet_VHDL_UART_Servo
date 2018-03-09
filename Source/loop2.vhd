95c733e5edfa21bc71ad97a7801ddcb31c06a21d
TimeEngineer <36041853+TimeEngineer@users.noreply.github.com>
2018-03-09 19:42:26 +0100

    back up

--- Source/loop2.vhd
+++ Source/loop2.vhd
@@ -2,7 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity loop2 is port(
-  clk    	: in std_logic;
+  tick     	: in std_logic;
   input   	: in std_logic_vector (7 downto 0);
   dataValid	: in std_logic;
   rst		: in std_logic;
@@ -11,18 +11,20 @@ entity loop2 is port(
 end loop2;
 
 architecture behav of loop2 is
+signal reg 	: std_logic_vector (7 downto 0);
 
 begin
-clocked : process(clk, rst)
+clocked : process(tick, rst)
 begin
   if (rst = '1') then
     start <= '0';
-  elsif (clk'event and clk = '1') then
+    reg <= (others => '0');
+  elsif (tick'event and tick = '1') then
     if (dataValid = '1') then
-      output <= input;
+      reg <= input;
       start <= '1';
     end if;
-    start <= '0';
+    output <= reg;
   end if;
 end process clocked;
 end behav;
