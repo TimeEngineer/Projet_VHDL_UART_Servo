e1a4c08f3cb24b252c1c2ea3ea64b98b59ed0634
TimeEngineer <36041853+TimeEngineer@users.noreply.github.com>
2018-03-09 19:29:39 +0100

    bug fix

--- Source/loop1.vhd
+++ Source/loop1.vhd
@@ -2,8 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity loop1 is port(
-  clk     	: in std_logic;
-  tick		: in std_logic;  
+  clk    	: in std_logic;
   input   	: in std_logic_vector (7 downto 0);
   dataValid	: in std_logic;
   rst		: in std_logic;
@@ -17,12 +16,13 @@ begin
 clocked : process(clk, rst)
 begin
   if (rst = '1') then
-    start <= '0'
+    start <= '0';
   elsif (clk'event and clk = '1') then
     if (dataValid = '1') then
       output <= input;
       start <= '1';
     end if;
-    start <= '0'
+    start <= '0';
+  end if;
 end process clocked;
 end behav;
